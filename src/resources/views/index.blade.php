bb81af1bb10c6178ba90ddcebc55719dd1672e7a
marsheyxx36 <masshun36@gmail.com>
2022-07-31 18:17:23 +0900

    create_index.php

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -11,7 +11,55 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <header>
-
+        <div class="header-logo"></div>
+        <div class="header-title">KARAAGE MANIA</div>
+        <nav>
+            <ul class="header-nav">
+                <li class="nav-item">最新投稿一覧</li>
+                <li class="nav-item">地域から探してみる</li>
+                <li class="nav-item">タイプで探してみる</li>
+                <li class="nav-item">ログイン</li>
+                <li class="nav-item">新規登録</li>
+            </ul>
+        </nav>
     </header>
+    <main>
+        <section>
+        <div class="main-image">
+                <img src="{{asset('images/PAK88_syokutakukaraage20150203190242.jpg')}}" alt="" width="1000px" height="800px">
+        </div>
+        <h1>あなたの食べたいからあげにきっと出会えるサイト</h1>
+        </section>
+
+        <section>
+            <h2>KARAAGE MANIAとは？</h1>
+            <p class="site-description">
+                ーKARAAGE MANIAは唐揚げの情報共有サイトになります。
+                この地域でおいしい唐揚げを探しているのに中々情報がないなぁ。。と
+                困ったことはありませんか？
+                ここには全国のユーザーより投稿された、おすすめの唐揚げが集まっています。
+                きっとあなたが食べてみたい唐揚げ出会えるはずです。
+                NO KARAGE , NO LIFE.
+                あなたの好みの唐揚げをぜひ探してみてください。
+            </p>
+        </section>    
+
+    </main>
+
+    <footer>
+    <div class="footer-left"></div>
+    <div class="footer-right">
+        <nav class="footer-nav">
+            <ul>
+                <li>お問い合わせ</li>
+                <li>最新投稿一覧</li>
+                <li>利用規約</li>
+                <li>地域から探してみる</li>
+                <li>プライバシーポリシー</li>
+                <li>タイプから探してみる</li>
+            </ul>
+        </nav>
+    </div>
+    </footer>
   </body>
 </html>
