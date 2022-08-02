db02413dd985fb6c184d5b2473c79dd44d4e4532
marsheyxx36 <masshun36@gmail.com>
2022-08-02 13:21:25 +0900

    fix

---
+++ src/resources/views/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="header-left">
+        <img src="{{asset('images/icon2.png')}}" alt="" class="header-logo">
+        <div class="header-title">KARAAGE MANIA</div>
+    </div>
+ 
+    <nav class="header-right">
+        <ul class="header-nav">
+            <li class="header-nav-item">最新投稿一覧</li>
+            <li class="header-nav-item">地域から探してみる</li>
+            <li class="header-nav-item">タイプで探してみる</li>
+            <li class="header-nav-item">ログイン</li>
+            <li class="header-nav-item">新規登録</li>
+        </ul>
+    </nav>
+    
+</header>
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,34 +1,7 @@
-<!doctype html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Karaage Mania</title>
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-  </head>
-  <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <header>
-        <div class="header-left">
-            <img src="{{asset('images/icon2.png')}}" alt="" class="header-logo">
-            <div class="header-title">KARAAGE MANIA</div>
-        </div>
-     
-        <nav class="header-right">
-            <ul class="header-nav">
-                <li class="header-nav-item">最新投稿一覧</li>
-                <li class="header-nav-item">地域から探してみる</li>
-                <li class="header-nav-item">タイプで探してみる</li>
-                <li class="header-nav-item">ログイン</li>
-                <li class="header-nav-item">新規登録</li>
-            </ul>
-        </nav>
-        
-    </header>
-    <main class="container-fluid">
+ @extends('layouts.app')
+ @section('content')
+ <main>
         <section>
         <div class="main-capture">
                 <img src="{{asset('images/PAK88_syokutakukaraage20150203190242_TP_V4.jpg')}}" alt="" class="main-image">
@@ -56,23 +29,5 @@
             <button class="site-description-button">唐揚げを探してみる！</button>
         </section>    
 
-    </main>
-
-    <footer>
-    <div class="footer-left">
-        <img src="{{asset('images/icon2.png')}}" alt="" class="header-logo">
-            <div class="header-title">KARAAGE MANIA</div>
-    </div>
-    <div class="footer-right">
-            <ul class="footer-nav">
-                <li class="footer-nav-item">お問い合わせ</li>
-                <li class="footer-nav-item">最新投稿一覧</li>
-                <li class="footer-nav-item">利用規約</li>
-                <li class="footer-nav-item">地域から探してみる</li>
-                <li class="footer-nav-item">プライバシーポリシー</li>
-                <li class="footer-nav-item">タイプから探してみる</li>
-            </ul>
-    </div>
-    </footer>
-  </body>
-</html>
+</main>
+@endsection
