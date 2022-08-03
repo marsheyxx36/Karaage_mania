3c579d7c2156031b488a2f64823dfb1ae6c0d25f
marsheyxx36 <masshun36@gmail.com>
2022-08-03 21:08:00 +0900

    rename index

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/index', function () {
-    return view('index');
+Route::get('/toppage', function () {
+    return view('toppage');
 });
