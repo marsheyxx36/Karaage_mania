884616f854b5fa0ee99b70086335fc61225a3d03
marsheyxx36 <masshun36@gmail.com>
2022-08-07 13:11:05 +0900

    fix

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<main>
+<main class="container">
     <div class="posts-title">
         <img src="{{asset('images/icon1.png')}}" alt="" class="posts-title-logo">
         <h2>みんなの最新投稿一覧</h2>
@@ -23,10 +23,67 @@
                 <p class="post-description">
                     説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。<br>
                     説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。<br>
-                    説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。<br>
-                    
                 </p>
             </div>
         </div>
+
+        <div class="post">
+            <div class="post-images">
+                <img src="images/IK21317949_TP_V4.jpg" class="post-image" >
+            </div>
+
+            <div class="post-content">
+                <h3 class="post-title">店舗名：サンプルデータです。</h3>
+                <ul class="post-category">
+                    <li class="post-category-item">エリア：関東/東京</li>
+                    <li class="post-category-item">部位：むね肉</li>
+                    <li class="post-category-item">タイプ：にんにく醤油系</li>
+                </ul>
+                <p class="post-description">
+                    説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。<br>
+                    説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。<br>
+                </p>
+            </div>
+        </div>
+
+        <div class="post">
+            <div class="post-images">
+                <img src="images/IK21317949_TP_V4.jpg" class="post-image" >
+            </div>
+
+            <div class="post-content">
+                <h3 class="post-title">店舗名：サンプルデータです。</h3>
+                <ul class="post-category">
+                    <li class="post-category-item">エリア：関東/東京</li>
+                    <li class="post-category-item">部位：むね肉</li>
+                    <li class="post-category-item">タイプ：にんにく醤油系</li>
+                </ul>
+                <p class="post-description">
+                    説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。<br>
+                    説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。<br>
+                </p>
+            </div>
+        </div>
+
+        <div class="post">
+            <div class="post-images">
+                <img src="images/IK21317949_TP_V4.jpg" class="post-image" >
+            </div>
+
+            <div class="post-content">
+                <h3 class="post-title">店舗名：サンプルデータです。</h3>
+                <ul class="post-category">
+                    <li class="post-category-item">エリア：関東/東京</li>
+                    <li class="post-category-item">部位：むね肉</li>
+                    <li class="post-category-item">タイプ：にんにく醤油系</li>
+                </ul>
+                <p class="post-description">
+                    説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。<br>
+                    説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。<br>
+                </p>
+            </div>
+        </div>
+
+        
     </div>
 </main>
