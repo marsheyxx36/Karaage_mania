58c42288f4a0c572cae9977bba00e4b1c50e175c
marsheyxx36 <masshun36@gmail.com>
2022-08-16 01:27:57 +0900

    Update 2022_08_13_110650_create_posts_table.php

--- src/database/migrations/2022_08_13_110650_create_posts_table.php
+++ src/database/migrations/2022_08_13_110650_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
             $table->string('store_name');
             $table->string('area');
             $table->string('address');
