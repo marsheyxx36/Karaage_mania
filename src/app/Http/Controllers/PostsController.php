65cf8dab8d9b18b88a52a666eaf3d954dafcd4d0
marsheyxx36 <masshun36@gmail.com>
2022-08-16 01:28:04 +0900

    Update PostsController.php

--- src/app/Http/Controllers/PostsController.php
+++ src/app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Post;
 class PostsController extends Controller
 {
@@ -18,14 +19,40 @@ class PostsController extends Controller
     }
 
     public function store(Request $request){
+        $validate_rule =[
+            'store_name'=>'required',
+            'area'=> 'required',
+            'address'=> 'required',
+            'type'=> 'required',
+            'comment'=> 'required',
+            'item'=> 'required',
+            'image'=> 'required',
+        ];
         $post = new Post;
+        $user_id = Auth::id();
         $post->store_name = $request->store_name;
+        $post->user_id = $user_id;
         $post->area = $request->area;
         $post->address = $request->address;
         $post->type = $request->type;
         $post->comment = $request->comment;
+        $post->item = $request->item;
         $post->image = $request->image;
+        
+        $this->validate($request, $validate_rule);
+        if($file = $request->image){
+            
+               $fileName = time().'.'.$file->getClientOriginalExtension();
+               $target_path = public_path('/uploads/');
+               $file->move($target_path,$fileName);
+                   
+             }else{
+               $name = "";
+             }
+
         $post->save();
+
+       
         return redirect('/toppage');
     }
 }
