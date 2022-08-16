b3388289fbb06e9a972cb60d2886d6b02366d95b
marsheyxx36 <masshun36@gmail.com>
2022-08-16 11:39:46 +0900

    Update PostRequest.php

--- src/app/Http/Requests/PostRequest.php
+++ src/app/Http/Requests/PostRequest.php
@@ -13,7 +13,12 @@ class PostRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        if($this->path() == 'post'){
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 
     /**
@@ -24,7 +29,26 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+           
+                'store_name'=>'required',
+                'area'=> 'required',
+                'address'=> 'required',
+                'type'=> 'required',
+                'comment'=> 'required',
+                'item'=> 'required',
+                'image'=> 'required',
+        ];
+    }
+
+    public function messages(){
+        return[
+            'store_name.required' => '店舗名は必ず記入してください',
+            'area.required' => '地域はは必ず選択してください',
+            'address.required' => '住所は必ず記入してください',
+            'type.required' => '唐揚げのタイプは必ず記入してください',
+            'comment.required' => 'コメントは必ず記入してください',
+            'item.required' => '商品名は必ず記入してください',
+            'image.required' => '画像を選択してください',
         ];
     }
 }
